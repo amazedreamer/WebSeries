b656221b70e5898546839fe3060937e722aecd99
amazedreamer <68944068+amazedreamer@users.noreply.github.com>
2026-07-15 08:13:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,8 +63,8 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "alpha-links.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "483cba84c70761bfb95ec7a528478b00c348c41f")
 
 # Shortener 2
-SHORTLINK_URL_2 = os.environ.get("SHORTLINK_URL_2", "urllinkshort.in")
-SHORTLINK_API_2 = os.environ.get("SHORTLINK_API_2", "f06d773b5855747d387da0b37c28c963a142d808")
+SHORTLINK_URL_2 = os.environ.get("SHORTLINK_URL_2", "short-pay.xyz")
+SHORTLINK_API_2 = os.environ.get("SHORTLINK_API_2", "c3c8be6b4d487afbafeaf878e452f6572217692d")
 
 # Shortener 3
 SHORTLINK_URL_3 = os.environ.get("SHORTLINK_URL_3", "lnkbit.fyi")
